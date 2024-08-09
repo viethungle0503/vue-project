0d110ec4f97bbe82804a6d04e644f3ad18920f76
viethungle0503 <viethungitmi@gmail.com>
2024-08-09 13:56:18 +0700

    Add PageViewer component and axios package to the project

---
+++ src/components/PageViewer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  pageTitle: String,
+  content: String
+})
+</script>
+
+<template>
+  <div id="content" class="container">
+    <h1>{{ pageTitle }}</h1>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+import PageViewer from '../components/PageViewer.vue'
 const activePage = ref<number>(0)
+const useDarkNavBar = ref<boolean>(true)
+const navbarClass = computed(() => {
+  return {
+    'navbar-light': !useDarkNavBar.value,
+    'bg-light': !useDarkNavBar.value,
+    'navbar-dark': useDarkNavBar.value,
+    'bg-dark': useDarkNavBar.value
+  }
+})
+const theme = computed(() => {
+  return useDarkNavBar.value ? 'dark' : 'light'
+})
+
 const pages = [
   {
     link: { text: 'Home', url: '/home' },
@@ -20,18 +34,24 @@ const pages = [
     content: 'This is the contact content'
   }
 ]
+
+// v-bind="activePage === index ? { class: 'nav-link active' } : { class: 'nav-link' }"
+// :class="{ 'navbar-light bg-light': !useDarkNavBar, 'navbar-dark bg-dark': useDarkNavBar }"
+// :class="navbarClass"
+// :class="[useDarkNavBar ? 'navbar-dark bg-dark' : 'navbar-light bg-light','navbar', 'navbar-expand-lg']"
 </script>
 
 <template>
   <main>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <nav :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">My Vue</a>
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li v-for="(page, index) in pages" class="nav-item" v-bind:key="index">
             <a
               v-bind:href="page.link.url"
-              v-bind="activePage === index ? { class: 'nav-link active' } : { class: 'nav-link' }"
+              class="nav-link"
+              :class="{ active: activePage == index }"
               aria-current="page"
               :title="`This link goes to somewhere ${page.link.text}`"
               @click.prevent="activePage = index"
@@ -39,9 +59,16 @@ const pages = [
             >
           </li>
         </ul>
+        <form class="d-flex">
+          <button class="btn btn-primary" @click.prevent="useDarkNavBar = !useDarkNavBar">
+            Toggle Navbar
+          </button>
+        </form>
       </div>
     </nav>
-    <h1>{{ pages[activePage].pageTitle }}</h1>
-    <p>{{ pages[activePage].content }}</p>
+    <PageViewer
+      v-bind:pageTitle="pages[activePage].pageTitle"
+      :content="pages[activePage].content"
+    />
   </main>
 </template>
