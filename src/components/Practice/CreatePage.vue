feef5ccca1e749c4cdc9514c9a34b82a77552d83
viethungle0503 <viethungitmi@gmail.com>
2024-08-11 22:03:32 +0700

    Disable submit button if form fields is not filled

--- src/components/Practice/CreatePage.vue
+++ src/components/Practice/CreatePage.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+
+const props = defineProps<{
+  pageCreated: Function
+}>()
+
+const isFormValid = computed(() => {
+    return !pageTitle.value || !content.value || !linkText.value || !linkUrl.value
+})
 
 const pageTitle = ref<string>('')
 const content = ref<string>('')
-defineProps<{
-  pageCreated: Function
-}>()
+const linkText = ref<string>('')
+const linkUrl = ref<string>('')
+const submitForm = () => {
+  if (!pageTitle.value || !content.value || !linkText.value || !linkUrl.value) {
+    alert('Please fill out the form')
+    return
+  }
+  props.pageCreated({
+    pageTitle: pageTitle.value,
+    content: content.value,
+    link: {
+      text: linkText.value,
+      url: linkUrl.value
+    }
+  })
+}
 </script>
 
 <!-- 
@@ -22,37 +43,35 @@ defineProps<{
   <div class="container mb-3">
     <form action="">
       <div class="row">
-          <div class="col-md-8">
-              <div class="mb-3">
-                <label for="" class="form-label">Page Title</label>
-                <input type="text" class="form-control" v-model="pageTitle" />
-              </div>
-              <div class="mb-3">
-                <label for="" class="form-label">Content</label>
-                <textarea type="text" class="form-control" rows="5" v-model="content"></textarea>
-              </div>
-              <div class="mb-3">
-                <button class="btn btn-primary" @click.prevent="pageCreated({ pageTitle, content })">
-                  Create Page
-                </button>
-              </div>
+        <div class="col-md-8">
+          <div class="mb-3">
+            <label for="" class="form-label">Page Title</label>
+            <input type="text" class="form-control" v-model="pageTitle" />
           </div>
-          <div class="col">
-              <div class="mb-3">
-                <label for="" class="form-label">Link Text</label>
-                <input type="text" class="form-control"/>
-              </div>
-              <div class="mb-3">
-                <label for="" class="form-label">Link URL</label>
-                <input type="text" class="form-control"/>
-              </div>
-              <div class="row mb-3">
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="form-check-label">Published</label>
-                </div>
-              </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Content</label>
+            <textarea type="text" class="form-control" rows="5" v-model="content"></textarea>
           </div>
+          <div class="mb-3">
+            <button class="btn btn-primary" @click.prevent="submitForm" :disabled="isFormValid">Create Page</button>
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-3">
+            <label for="" class="form-label">Link Text</label>
+            <input type="text" class="form-control" v-model="linkText" />
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Link URL</label>
+            <input type="text" class="form-control" v-model="linkUrl" />
+          </div>
+          <div class="row mb-3">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" />
+              <label class="form-check-label">Published</label>
+            </div>
+          </div>
+        </div>
       </div>
     </form>
   </div>
