f6d708314bc5a4cda8da6efdb6dc2bfe99429ca3
viethungle0503 <viethungitmi@gmail.com>
2024-09-23 15:23:22 +0700

    Uninstall cors-anywhere; install vite-plugin-checker and vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import nightwatchPlugin from 'vite-plugin-nightwatch'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +21,11 @@ export default defineConfig({
       resolvers: [
         PrimeVueResolver()
       ]
-    })
+    }),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
   ],
   resolve: {
     alias: {
